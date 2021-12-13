d0a3308472e555013e0dde2113985b431a1d5aad
jsankw <48301268+jsankw@users.noreply.github.com>
2021-12-13 13:16:39 +0900

    Initial commit of files

---
+++ lambda/constants.js
@@ -0,0 +1,20 @@
+exports.audioData = [
+];
+
+/** Example format
+exports.audioData = [
+  {
+    title: 'Title',
+    url: 'https://domain.com/test.mp3',
+  },
+  {
+    title: 'Title 2',
+    url: 'https://domain.com/test2.mp3',
+  },
+  {
+    title: 'Title 3',
+    url: 'https://domain.com/test3.mp3',
+  }
+];
+
+**/
